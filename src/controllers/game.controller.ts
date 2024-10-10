fc70500a1c1b0f8511687159897a850352d539cc
Ediz Buba <ediz.buba@gmail.com>
2024-10-10 16:53:38 +0200

    3 terminé

--- src/controllers/game.controller.ts
+++ src/controllers/game.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Get, Route, Tags } from "tsoa";
+import { Controller, Get, Post, Delete, Route, Path, Body, Tags, Patch } from "tsoa";
 import { GameDTO } from "../dto/game.dto";
+import { CreateGameDTO } from "../dto/creategame.dto";
 import { gameService } from "../services/game.service";
 
 @Route("games")
@@ -8,5 +9,28 @@ export class GameController extends Controller {
   @Get("/")
   public async getAllGames(): Promise<GameDTO[]> {
     return gameService.getAllGames();
+  }
+
+  @Get("{id}")
+  public async getGameById(@Path() id: number): Promise<GameDTO> {
+    return gameService.getGameById(id);
+  }
+
+  @Post("/")
+  public async createGame(@Body() requestBody: CreateGameDTO): Promise<GameDTO> {
+    const { title, consoleId } = requestBody;
+
+    if (!consoleId || typeof consoleId !== 'number') {
+      throw new Error("Console ID is required and must be a number");
+    }
+    return gameService.createGame(title, consoleId); 
+  }
+
+  @Patch("{id}")
+  public async updateGame(
+    @Path() id: number,
+    @Body() requestBody: CreateGameDTO
+  ): Promise<GameDTO | null> {
+    return gameService.updateGame(id, requestBody);
   }
 }
--- src/services/game.service.ts
+++ src/services/game.service.ts
@@ -1,10 +1,12 @@
 import { GameDTO } from "../dto/game.dto";
 import { Console } from "../models/console.model";
 import { Game } from "../models/game.model";
+import { notFound } from "../error/NotFoundError";
+import { CreateGameDTO } from "../dto/creategame.dto";
 
 export class GameService {
   public async getAllGames(): Promise<GameDTO[]> {
-    return Game.findAll({
+    const games = await Game.findAll({
       include: [
         {
           model: Console,
@@ -12,6 +14,71 @@ export class GameService {
         },
       ],
     });
+    
+    return games.map(game => ({
+      id: game.id,
+      title: game.title,
+      console: {
+        id: game.console.id,
+        name: game.console.name,
+        manufacturer: game.console.manufacturer
+      }
+    }));
+  }
+
+  public async getGameById(id: number): Promise<GameDTO> {
+    const game = await Game.findByPk(id, {
+      include: [
+        {
+          model: Console,
+          as: "console",
+        },
+      ],
+    });
+
+    if (!game) {
+      notFound(`Game with id ${id}`); 
+    }
+    
+
+    return {
+      id: game.id,
+      title: game.title,
+      console: {
+        id: game.console.id,
+        name: game.console.name,
+        manufacturer: game.console.manufacturer,
+      }
+    };
+  }
+
+  public async createGame(
+    title: string, 
+    consoleId: number
+  ): Promise<GameDTO> {
+    const consoleExists = await Console.findByPk(consoleId);
+    if (!consoleExists) {
+      notFound(`Console with id ${consoleId} does not exist`);
+    }
+
+    return Game.create({ title, console_id: consoleId });
+  }
+  
+  public async updateGame(id: number, dto: CreateGameDTO): Promise<GameDTO | null> {
+    const game = await Game.findByPk(id);
+    if (!game) {
+      notFound(`Game with id ${id}`);
+    }
+
+    const consoleExists = await Console.findByPk(dto.consoleId);
+    if (!consoleExists) {
+      throw new Error(`Console with id ${dto.consoleId} does not exist`);
+    }
+    game.title = dto.title;
+    game.console_id = dto.consoleId;
+    await game.save();
+
+    return game;
   }
 }
 
